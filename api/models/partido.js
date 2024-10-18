649f75ae1901b4d1ecdfe248ae44380ce002ee34
aperezmarquez <aperezmarquez.business@gmail.com>
2024-10-18 19:09:32 +0200

    En partido, implementadas las variables jugadores_local y visitante

--- api/models/partido.js
+++ api/models/partido.js
@@ -15,11 +15,21 @@ const PartidoSchema = new mongoose.Schema({
         ref: 'Equipo',
         required: true,
     },
+    jugadores_local: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Jugador',
+        required: true,
+    }],
     equipo_visitante: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Equipo',
         required: true,
     },
+    jugadores_visitante: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Jugador',
+        required: true,
+    }],
     goles: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Gol'
