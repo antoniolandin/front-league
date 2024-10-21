76153e935bf69654ba75a0783883ca978bd836ee
antoniolandin <antoniolandin@proton.me>
2024-10-21 11:39:07 +0200

    fix(api): index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,1 @@
 const app = require('./app')
-const colors = require('colors')
-
-// El puerto es obtenido desde las variables de entorno, si no existe, se usa el puerto 3000
-const port = process.env.PORT ?? 3000
-
-// Escuchamos en el puerto
-app.listen(port, () => {
-    console.log("Servidor escuchando en el puerto ".magenta + port.toString().yellow)
-})
